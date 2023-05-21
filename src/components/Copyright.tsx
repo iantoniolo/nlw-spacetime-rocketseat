bec16750c049d2dc12ea336d4e0b2ab1fb34a5ab
iantoniolo <iantoniolo@hotmail.com>
2023-05-21 15:12:50 -0300

    frontend - aula final

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com amor no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noreferger noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://www.rocketseat.com.br/"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
