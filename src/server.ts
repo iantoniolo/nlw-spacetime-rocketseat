1067a799b5d4e8922d66ba4ffec487671214e2fc
iantoniolo <iantoniolo@hotmail.com>
2023-05-21 15:09:36 -0300

    backend - aula final

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,34 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-// HTTP Method: GET, POST, PUT, PATCH, DELETE
+app.register(multipart)
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-  return users
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
 })
+
+app.register(cors, {
+  origin: true, // todas URLs de front-end poderão acessar nosso back-end
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
